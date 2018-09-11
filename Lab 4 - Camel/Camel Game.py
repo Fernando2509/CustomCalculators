0c42f9c9857a2403d785c300a3bbf4e727be4476
Antonio Fernando <mrfernando2509@gmail.com>
2018-09-11 00:37:10 -0400

    Added Camel Game Skeleton

---
+++ Lab 4 - Camel/Camel Game.py
@@ -0,0 +1,24 @@
+#http://programarcadegames.com/index.php?chapter=lab_camel&lang=en
+class Player():
+    pass
+
+class Natives():
+    pass
+
+class Boart():
+    def __init__(self, desertSize, **kwargs):
+        self.desertSize = desertSize
+
+
+
+
+class Main():
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)
+
+if __name__ == '__main__':
+    Main()
+
+
+
+
