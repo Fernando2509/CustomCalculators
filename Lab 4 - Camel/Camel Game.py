ef8a7a0e51e6f57b31156847756cca1eee1fb40d
Antonio Fernando <mrfernando2509@gmail.com>
2018-09-11 10:06:13 -0400

    Added more methods to the game class

--- Lab 4 - Camel/Camel Game.py
+++ Lab 4 - Camel/Camel Game.py
@@ -1,9 +1,14 @@
 #http://programarcadegames.com/index.php?chapter=lab_camel&lang=en
+from os import system 
+import math
 
 class Game():
     def __init__(self, desertSize, **kwargs):
         self.desertSize = desertSize
         self.escaped = False
+        self.player = 0
+        self.natives = -20
+        self.drinks = 5
         print("Welcome to Camel!")
         print("You have stolen a camel to make your way across the great Mobi desert.")
         print("The natives want their camel back and are chasing you down! Survive your")
@@ -11,12 +16,13 @@ class Game():
         
 
     def Start(self):
-        self.player = 0
-        self.natives =0 
         while not self.escaped:
+            #Clear Console
+            system('cls') 
             self.choice = self.AskChoices()
     
     def AskChoices(self):
+        #TODO Create an action class that has a label and a "action" method
         print("")
         print("A. Drink from your canteen.")
         print("B. Ahead moderate speed.")
@@ -34,6 +40,14 @@ class Game():
             choice = input("What do you want to do?")
             if choice.upper() not in available_options:
                 print("Invalid Choice, please try again\r\n")
+
+    def Status(self):
+        print("Miles traveled:  {}".format(self.player))
+        print("Drinks in canteen:  {}".format(self.drinks))
+        print("The natives are 10 miles behind you.".format(math.abs(self.player - self.natives)))
+
+    def DrawMap(self):
+        #TODO DrawMap
 
 class Main():
     def __init__(self, *args, **kwargs):
